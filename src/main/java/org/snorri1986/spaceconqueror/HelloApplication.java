8761c93d707b7c5a11b43605f0c1da0f6f1cabe2
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-08-29 22:58:51 +0200

    refactored for better testing

--- src/main/java/org/snorri1986/spaceconqueror/HelloApplication.java
+++ src/main/java/org/snorri1986/spaceconqueror/HelloApplication.java
@@ -10,11 +10,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Scene scene = setScene();
+        initStage(stage, scene);
+        stage.show();
+    }
+
+    Scene setScene() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        return scene;
+    }
+
+    Stage initStage(Stage stage,Scene scene) {
         stage.setTitle("Hello!");
         stage.setScene(scene);
-        stage.show();
+        return stage;
     }
 
     public static void main(String[] args) {
