9bc3b7571f9606be34566d759976e11055a5aa4f
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-09-15 23:26:21 +0200

    #5 working under test methods(not finished)

--- src/test/java/org/snorri1986/spaceconqueror/AppLauncherControllerTest.java
+++ src/test/java/org/snorri1986/spaceconqueror/AppLauncherControllerTest.java
@@ -1,18 +1,23 @@
 package org.snorri1986.spaceconqueror;
 
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 
 
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(ApplicationExtension.class)
@@ -27,11 +32,14 @@ public class AppLauncherControllerTest extends ApplicationTest {
     }
     @Test
     @Disabled // body will be used in future
+    //TODO: method is not finished
     void checkBtnSolarSystem() {
         Button btnSolarSystem = lookup("#btn_s_system").queryButton();
         clickOn(btnSolarSystem);
+//        Stage stageSolarSystem = (Stage) btnSolarSystem.getScene().getWindow();
+//        String titleWindowSolarSystem = stageSolarSystem.getTitle();
 //        Label labSolarSystem = lookup("#SolarSystem").query();
-//        assertEquals("SolarSystem",labSolarSystem.getText());
+//        assertEquals("SolarSystem",titleWindowSolarSystem);
     }
 
     @Test
