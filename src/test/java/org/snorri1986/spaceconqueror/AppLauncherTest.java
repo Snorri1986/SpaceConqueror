e2c4742e920bc116fddf6ff4904aa5e803e28370
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-09-20 14:40:16 +0200

    #9 unit tests to checking images on buttons

--- src/test/java/org/snorri1986/spaceconqueror/AppLauncherTest.java
+++ src/test/java/org/snorri1986/spaceconqueror/AppLauncherTest.java
@@ -1,5 +1,6 @@
 package org.snorri1986.spaceconqueror;
 
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -43,10 +44,24 @@ public class AppLauncherTest extends ApplicationTest {
         assertEquals("Solar System", btn.getText());
     }
     @Test
+    void checkGraphicOnButtonSolarSystem() throws IOException {
+        Scene originalScene = hello.setScene();
+        Button btn = (Button)originalScene.lookup("#btn_s_system");
+        Node picSun = btn.getGraphic();
+        assertNotNull(picSun);
+    }
+    @Test
     void checkTextOnButtonSovietUnion() throws IOException {
         Scene originalScene = hello.setScene();
         Button btn = (Button)originalScene.lookup("#btn_s_union");
         assertEquals("Soviet Union", btn.getText());
+    }
+    @Test
+    void checkGraphicOnButtonSovietUnion() throws IOException {
+        Scene originalScene = hello.setScene();
+        Button btn = (Button)originalScene.lookup("#btn_s_union");
+        Node picSovietUnion = btn.getGraphic();
+        assertNotNull(picSovietUnion);
     }
     @Test
     void checkTextOnButtonChina() throws IOException {
@@ -55,10 +70,24 @@ public class AppLauncherTest extends ApplicationTest {
         assertEquals("China", btn.getText());
     }
     @Test
+    void checkGraphicOnButtonChina() throws IOException {
+        Scene originalScene = hello.setScene();
+        Button btn = (Button)originalScene.lookup("#btn_china");
+        Node picChina = btn.getGraphic();
+        assertNotNull(picChina);
+    }
+    @Test
     void checkTextOnButtonUSA() throws IOException {
         Scene originalScene = hello.setScene();
         Button btn = (Button)originalScene.lookup("#btn_usa");
         assertEquals("USA", btn.getText());
+    }
+    @Test
+    void checkGraphicOnButtonUSA() throws IOException {
+        Scene originalScene = hello.setScene();
+        Button btn = (Button)originalScene.lookup("#btn_usa");
+        Node picUSA = btn.getGraphic();
+        assertNotNull(picUSA);
     }
     @Test
     void checkTextOnButtonIndia() throws IOException {
@@ -67,9 +96,23 @@ public class AppLauncherTest extends ApplicationTest {
         assertEquals("India(Bharat)", btn.getText());
     }
     @Test
+    void checkGraphicOnButtonIndia() throws IOException {
+        Scene originalScene = hello.setScene();
+        Button btn = (Button)originalScene.lookup("#btn_ind_bharat");
+        Node picIndia = btn.getGraphic();
+        assertNotNull(picIndia);
+    }
+    @Test
     void checkTextOnButtonSpaceX() throws IOException {
         Scene originalScene = hello.setScene();
         Button btn = (Button)originalScene.lookup("#btn_space_x");
         assertEquals("Space X", btn.getText());
+    }
+    @Test
+    void checkGraphicOnButtonSpaceX() throws IOException {
+        Scene originalScene = hello.setScene();
+        Button btn = (Button)originalScene.lookup("#btn_space_x");
+        Node picSpaceX = btn.getGraphic();
+        assertNotNull(picSpaceX);
     }
 }
