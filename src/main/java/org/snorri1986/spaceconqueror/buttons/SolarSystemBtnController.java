832f7e741239583d7c648b7663d7dc5bc9afbaff
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-09-26 15:41:26 +0200

    #14 default buttons location

--- src/main/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnController.java
@@ -1,4 +1,25 @@
 package org.snorri1986.spaceconqueror.buttons;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SolarSystemBtnController {
+    @FXML
+    public Button mercury;
+    @FXML
+    public Button venus;
+    @FXML
+    public Button earth;
+    @FXML
+    public Button mars;
+    @FXML
+    public Button jupiter;
+    @FXML
+    public Button saturn;
+    @FXML
+    public Button uranus;
+    @FXML
+    public Button neptune;
+    @FXML
+    public Button pluto;
 }
