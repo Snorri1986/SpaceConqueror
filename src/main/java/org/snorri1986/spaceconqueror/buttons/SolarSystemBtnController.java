75085d5f422729bd86600b92593e55864753e2ee
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-09-29 15:12:32 +0200

    #14 handlers for btn Mercury

--- src/main/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnController.java
@@ -1,25 +1,63 @@
 package org.snorri1986.spaceconqueror.buttons;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.snorri1986.spaceconqueror.AppLauncherController;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class SolarSystemBtnController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SolarSystemBtnController.class);
     @FXML
     public Button mercury;
+//    @FXML
+//    public Button venus;
+//    @FXML
+//    public Button earth;
+//    @FXML
+//    public Button mars;
+//    @FXML
+//    public Button jupiter;
+//    @FXML
+//    public Button saturn;
+//    @FXML
+//    public Button uranus;
+//    @FXML
+//    public Button neptune;
+//    @FXML
+//    public Button pluto;
+
     @FXML
-    public Button venus;
-    @FXML
-    public Button earth;
-    @FXML
-    public Button mars;
-    @FXML
-    public Button jupiter;
-    @FXML
-    public Button saturn;
-    @FXML
-    public Button uranus;
-    @FXML
-    public Button neptune;
-    @FXML
-    public Button pluto;
+    protected void onBtnMercuryClick() {
+        try {
+            Scene sceneMercury = setMercuryScene();
+            Stage stageSolarSys = setMercuryStage(sceneMercury);
+            stageSolarSys.show();
+        } catch (IOException e) {
+            LOG.error("The button Mercury does not work",e);
+        }
+    }
+
+
+    Scene setMercuryScene() throws IOException {
+        URL fxmlLocationMercury = getClass().getResource("mercury.fxml");
+        FXMLLoader fxmlMercuryLoader = new FXMLLoader(fxmlLocationMercury);
+        Scene mercury = new Scene(fxmlMercuryLoader.load(), 600, 400);
+        return mercury;
+    }
+
+
+    Stage setMercuryStage(Scene scene) throws IOException {
+        Stage stage = new Stage();
+        stage.setTitle("Mercury");
+        stage.setScene(scene);
+        return stage;
+    }
 }
