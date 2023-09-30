327877726c7b29d531d9615522e3dbfafc373847
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-09-30 14:52:22 +0200

    #14 unit test(not completed) to btnMercury

--- src/test/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnControllerTest.java
+++ src/test/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnControllerTest.java
@@ -1,5 +1,7 @@
 package org.snorri1986.spaceconqueror.buttons;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
@@ -7,32 +9,35 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.snorri1986.spaceconqueror.AppLauncher;
 import org.snorri1986.spaceconqueror.AppLauncherController;
 import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.base.WindowMatchers;
 
 import java.io.IOException;
+import java.net.URL;
 
 @ExtendWith(ApplicationExtension.class)
 public class SolarSystemBtnControllerTest extends ApplicationTest {
 
     //TODO: config test class
 
-    private final AppLauncher hello = new AppLauncher();
-    private final SolarSystemBtnController solarSystemBtnController = new SolarSystemBtnController();
-
     @Start
     public void start(Stage stageOriginal) throws IOException {
-        hello.start(stageOriginal);
+        URL fxmlLocationSolarSystem = getClass().getResource("solar-system.fxml");
+        FXMLLoader fxmlSolarSystem = new FXMLLoader(fxmlLocationSolarSystem);
+        Scene solarSystem = new Scene(fxmlSolarSystem.load(), 600, 400);
+        stageOriginal.setTitle("About Solar System");
+        stageOriginal.setScene(solarSystem);
+        stageOriginal.show();
     }
 
     @Test
     void checkBtnMercury() {
-        Button solarSystem = lookup("#btn_s_system").queryButton();
         Button btnMercury = lookup("#mercury").queryButton();
-        clickOn(solarSystem);
         clickOn(btnMercury);
         FxAssert.verifyThat(window("Mercury"), WindowMatchers.isShowing());
     }
 }
+
