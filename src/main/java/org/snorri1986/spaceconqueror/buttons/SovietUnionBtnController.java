6d013edeaa7e9ae9a40cb232d46cfd602e555584
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-13 21:18:41 +0200

    #16 class controller for btn SovietUnion

---
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnController.java
@@ -0,0 +1,8 @@
+package org.snorri1986.spaceconqueror.buttons;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SovietUnionBtnController {
+    private static final Logger LOG = LoggerFactory.getLogger(SovietUnionBtnController.class);
+}
