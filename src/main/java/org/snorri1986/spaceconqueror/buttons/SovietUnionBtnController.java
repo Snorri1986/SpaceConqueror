30c97a2d4076b07e26b4b00864d696224c2c9e70
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-13 21:41:37 +0200

    #16 Buttons declarations

--- src/main/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnController.java
@@ -1,8 +1,29 @@
 package org.snorri1986.spaceconqueror.buttons;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SovietUnionBtnController {
     private static final Logger LOG = LoggerFactory.getLogger(SovietUnionBtnController.class);
+
+    @FXML
+    public Button vostok;
+    @FXML
+    public Button voshod;
+    @FXML
+    public Button soyuz;
+    @FXML
+    public Button mir;
+    @FXML
+    public Button buran;
+    @FXML
+    public Button luna;
+    @FXML
+    public Button venera;
+    @FXML
+    public Button vega;
+    @FXML
+    public Button mars;
 }
