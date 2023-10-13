99b86df59c8f54b2a5275ea97f17a43eb35d0be6
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-13 21:58:59 +0200

    #16 methods for Vostok button

--- src/main/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnController.java
@@ -1,9 +1,15 @@
 package org.snorri1986.spaceconqueror.buttons;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class SovietUnionBtnController {
     private static final Logger LOG = LoggerFactory.getLogger(SovietUnionBtnController.class);
@@ -26,4 +32,30 @@ public class SovietUnionBtnController {
     public Button vega;
     @FXML
     public Button mars;
+
+    @FXML
+    protected void onBtnVostokClick() {
+        try {
+            Scene sceneVostok = setVostokScene();
+            Stage stageVostok = setVostokStage(sceneVostok);
+            stageVostok.show();
+        } catch (IOException e) {
+            LOG.error("The button Vostok does not work",e);
+        }
+    }
+
+    Scene setVostokScene() throws IOException {
+        URL fxmlLocationVostok = getClass().getResource("vostok.fxml");
+        FXMLLoader fxmlVostokLoader = new FXMLLoader(fxmlLocationVostok);
+        Scene vostok = new Scene(fxmlVostokLoader.load(), 600, 400);
+        return vostok;
+    }
+
+
+    Stage setVostokStage(Scene scene) throws IOException {
+        Stage stage = new Stage();
+        stage.setTitle("Vostok");
+        stage.setScene(scene);
+        return stage;
+    }
 }
