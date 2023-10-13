bb5826a563c1f51ca4e0c1815cb056a9b0744f02
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-13 22:04:35 +0200

    #16 unit test for Vostok button

--- src/test/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnControllerTest.java
+++ src/test/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnControllerTest.java
@@ -1,6 +1,9 @@
 package org.snorri1986.spaceconqueror.buttons;
 
+import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.snorri1986.spaceconqueror.AppLauncherController;
 import org.testfx.framework.junit5.ApplicationExtension;
@@ -8,6 +11,8 @@ import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(ApplicationExtension.class)
 public class SovietUnionBtnControllerTest extends ApplicationTest {
@@ -19,5 +24,22 @@ public class SovietUnionBtnControllerTest extends ApplicationTest {
     public void start(Stage stageOriginal) throws IOException {
         stageOriginal = appLauncherController.setSovietUnionStage(appLauncherController.setSovietUnionScene());
         stageOriginal.show();
+    }
+
+    @Test
+    void testVostokStage() throws IOException {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Scene testVostokScene = null;
+                try {
+                    testVostokScene = sovietUnionBtnController.setVostokScene();
+                    Stage testVostokStage = sovietUnionBtnController.setVostokStage(testVostokScene);
+                    assertNotNull(testVostokStage);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
