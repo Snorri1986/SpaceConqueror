1ea72d6f0356c00c129f380f06cef1bade036c1d
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-17 15:08:19 +0200

    #17 define buttons

--- src/main/java/org/snorri1986/spaceconqueror/buttons/ChinaBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/ChinaBtnController.java
@@ -1,4 +1,38 @@
 package org.snorri1986.spaceconqueror.buttons;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ChinaBtnController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChinaBtnController.class);
+
+    @FXML
+    public Button prj9211;
+
+    @FXML
+    public Button prj9212;
+
+    @FXML
+    public Button prj9213;
+
+    @FXML
+    public Button lunar;
+
+    @FXML
+    public Button interplanet;
+
+    @FXML
+    public Button orbital;
+
+    @FXML
+    public Button marsPrg;
+
+    @FXML
+    public Button cosmodroms;
+
+    @FXML
+    public Button peoples;
 }
