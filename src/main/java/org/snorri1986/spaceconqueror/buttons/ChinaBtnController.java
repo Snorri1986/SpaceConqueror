9ac5eaf4524e56ad488c684f59f29f9525275e45
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-17 15:16:24 +0200

    #17 methods for PRJ9211 button

--- src/main/java/org/snorri1986/spaceconqueror/buttons/ChinaBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/ChinaBtnController.java
@@ -1,9 +1,15 @@
 package org.snorri1986.spaceconqueror.buttons;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ChinaBtnController {
 
@@ -35,4 +41,30 @@ public class ChinaBtnController {
 
     @FXML
     public Button peoples;
+
+    @FXML
+    protected void onBtnProject9211Click() {
+        try {
+            Scene scenePrj9211 = setPrj9211Scene();
+            Stage stagePrj9211 = setPrj9211Stage(scenePrj9211);
+            stagePrj9211.show();
+        } catch (IOException e) {
+            LOG.error("The button Project9211 does not work",e);
+        }
+    }
+
+    Scene setPrj9211Scene() throws IOException {
+        URL fxmlLocationPrj9211 = getClass().getResource("prj9211.fxml");
+        FXMLLoader fxmlPrj9211Loader = new FXMLLoader(fxmlLocationPrj9211);
+        Scene prj9211 = new Scene(fxmlPrj9211Loader.load(), 600, 400);
+        return prj9211;
+    }
+
+
+    Stage setPrj9211Stage(Scene scene) throws IOException {
+        Stage stage = new Stage();
+        stage.setTitle("PRJ9211");
+        stage.setScene(scene);
+        return stage;
+    }
 }
