8e1c30e87d4168211a5f1a86a7d964824e6ea43f
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-21 14:05:05 +0200

    #18 define buttons

--- src/main/java/org/snorri1986/spaceconqueror/buttons/UsaBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/UsaBtnController.java
@@ -1,4 +1,34 @@
 package org.snorri1986.spaceconqueror.buttons;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class UsaBtnController {
+
+    @FXML
+    public Button mercuryProject;
+
+    @FXML
+    public Button geminiProgram;
+
+    @FXML
+    public Button apolloProgram;
+
+    @FXML
+    public Button soyuzApollo;
+
+    @FXML
+    public Button iss;
+
+    @FXML
+    public Button spaceShuttle;
+
+    @FXML
+    public Button cosmodromsUSA;
+
+    @FXML
+    public Button people;
+
+    @FXML
+    public Button nasa;
 }
