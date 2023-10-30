3aa395d522417fb4fba910274da22d7712f6e16f
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-30 14:56:16 +0100

    #22 scene initialization

---
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/IndiaBtnController.java
@@ -0,0 +1,38 @@
+package org.snorri1986.spaceconqueror.buttons;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IndiaBtnController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IndiaBtnController.class);
+
+    @FXML
+    public Button satellites;
+
+    @FXML
+    public Button gagan;
+
+    @FXML
+    public Button irnss;
+
+    @FXML
+    public Button chandrayaanOne;
+
+    @FXML
+    public Button marsOrbiterMission;
+
+    @FXML
+    public Button chandrayaanThree;
+
+    @FXML
+    public Button adityaL1;
+
+    @FXML
+    public Button cosmodroms;
+
+    @FXML
+    public Button peoples;
+}
