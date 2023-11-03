d11776f9c0b1db757d3eb646cee902eaf92063bd
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-03 13:41:23 +0100

    #23 methods for FalconOne button

--- src/main/java/org/snorri1986/spaceconqueror/buttons/SpacexBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SpacexBtnController.java
@@ -1,9 +1,15 @@
 package org.snorri1986.spaceconqueror.buttons;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class SpacexBtnController {
 
@@ -35,4 +41,30 @@ public class SpacexBtnController {
 
     @FXML
     public Button peoples;
+
+    @FXML
+    protected void onBtnFalconOneClick() {
+        try {
+            Scene sceneFalconOne = setFalconOneScene();
+            Stage stageFalconOne = setFalconOneStage(sceneFalconOne);
+            stageFalconOne.show();
+        } catch (IOException e) {
+            LOG.error("The button Falcon One does not work",e);
+        }
+    }
+
+    Scene setFalconOneScene() throws IOException {
+        URL fxmlLocationFalconOne = getClass().getResource("falconone.fxml");
+        FXMLLoader fxmlFalconOneLoader = new FXMLLoader(fxmlLocationFalconOne);
+        Scene falconOne = new Scene(fxmlFalconOneLoader.load(), 600, 400);
+        return falconOne;
+    }
+
+
+    Stage setFalconOneStage(Scene scene) throws IOException {
+        Stage stage = new Stage();
+        stage.setTitle("Falcon One");
+        stage.setScene(scene);
+        return stage;
+    }
 }
