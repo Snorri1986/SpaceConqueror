1a1e26858ac6e5e81cb9b23584bf785c60b85318
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-03 13:31:52 +0100

    #23 define buttons

--- src/main/java/org/snorri1986/spaceconqueror/buttons/SpacexBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SpacexBtnController.java
@@ -1,4 +1,38 @@
 package org.snorri1986.spaceconqueror.buttons;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SpacexBtnController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SpacexBtnController.class);
+
+    @FXML
+    public Button falconone;
+
+    @FXML
+    public Button falconnine;
+
+    @FXML
+    public Button dragon;
+
+    @FXML
+    public Button falconheavy;
+
+    @FXML
+    public Button starship;
+
+    @FXML
+    public Button starlink;
+
+    @FXML
+    public Button elonmask;
+
+    @FXML
+    public Button cosmodroms;
+
+    @FXML
+    public Button peoples;
 }
