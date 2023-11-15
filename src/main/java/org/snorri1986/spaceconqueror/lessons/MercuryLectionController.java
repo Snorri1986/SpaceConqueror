e706e9cb0a79bcc6f33735963e5fd11a33e09ae1
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-15 21:02:44 +0100

    #28 can upload EN lection from file system

--- src/main/java/org/snorri1986/spaceconqueror/lessons/MercuryLectionController.java
+++ src/main/java/org/snorri1986/spaceconqueror/lessons/MercuryLectionController.java
@@ -1,18 +1,38 @@
 package org.snorri1986.spaceconqueror.lessons;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 
+public class MercuryLectionController implements Initializable {
 
-public class MercuryLectionController {
+    FileChooser fileChooserEN = new FileChooser();
 
     @FXML
-    Button btnLocalEn;
+    TextArea lessonTxt;
 
     @FXML
-    protected void onBtnLocalEnClick()  {
-
+    //TODO: add unit test
+    protected void onBtnLocalEnClick(MouseEvent event) throws FileNotFoundException {
+        File file = fileChooserEN.showOpenDialog(new Stage());
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()) {
+            lessonTxt.appendText(scanner.nextLine() + "\n");
+        }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fileChooserEN.setInitialDirectory(new File("D:\\Applications\\SpaceConqueror\\src\\main\\java\\org\\snorri1986\\spaceconqueror\\content\\text\\en"));
+    }
 }
