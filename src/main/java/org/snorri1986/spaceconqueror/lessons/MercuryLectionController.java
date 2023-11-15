b1ad920bf1c3f9483fe76e577d4b9c61ba6fd845
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-15 20:37:39 +0100

    #28 TextArea better than VBox. Also, moved resources to src

--- src/main/java/org/snorri1986/spaceconqueror/lessons/MercuryLectionController.java
+++ src/main/java/org/snorri1986/spaceconqueror/lessons/MercuryLectionController.java
@@ -2,27 +2,17 @@ package org.snorri1986.spaceconqueror.lessons;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 
-import java.io.*;
-import java.net.URISyntaxException;
-import java.util.Objects;
 
 
 public class MercuryLectionController {
 
     @FXML
-    VBox mercuryLection;
-
-    @FXML
     Button btnLocalEn;
 
     @FXML
-    protected void onBtnLocalEnClick() throws URISyntaxException {
-        String lessonText = Objects.requireNonNull(getClass().getResourceAsStream("src/main/resources/org/snorri1986/spaceconqueror/content/text/en/mercury_en.txt")).toString();
-        Label textLabel = new Label(lessonText);
-        mercuryLection.getChildren().add(textLabel);
+    protected void onBtnLocalEnClick()  {
+
     }
 
 }
