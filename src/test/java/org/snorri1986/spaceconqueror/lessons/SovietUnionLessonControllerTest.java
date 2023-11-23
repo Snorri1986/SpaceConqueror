ecbae74ab37dee5223d9a0f4057bf1113ca7e80e
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-23 21:05:50 +0100

    #30 unit tests for SovietUnion Space program and lessons file

---
+++ src/test/java/org/snorri1986/spaceconqueror/lessons/SovietUnionLessonControllerTest.java
@@ -0,0 +1,51 @@
+package org.snorri1986.spaceconqueror.lessons;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.snorri1986.spaceconqueror.AppLauncherController;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.Start;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@ExtendWith(ApplicationExtension.class)
+public class SovietUnionLessonControllerTest extends ApplicationTest {
+
+    public final AppLauncherController appLauncherController = new AppLauncherController();
+    FileChooser fileChooserUAtest = new FileChooser();
+    FileChooser fileChooserENtest = new FileChooser();
+
+    FileChooser fileChooserDKtest = new FileChooser();
+
+
+    @Start
+    public void start(Stage stageOriginal) throws IOException {
+        stageOriginal = appLauncherController.setSovietUnionStage(appLauncherController.setSovietUnionScene());
+        stageOriginal.show();
+    }
+
+    @Test
+    public void checkInitialDirectoryUA() {
+        fileChooserUAtest.setInitialDirectory(new File("D:\\Applications\\SpaceConqueror\\src\\main\\resources\\org\\snorri1986\\spaceconqueror\\content\\text\\sovietunion\\ua"));
+        assertNotNull(fileChooserUAtest.getInitialDirectory());
+    }
+
+    @Test
+    public void checkInitialDirectoryEN() {
+        fileChooserENtest.setInitialDirectory(new File("D:\\Applications\\SpaceConqueror\\src\\main\\resources\\org\\snorri1986\\spaceconqueror\\content\\text\\sovietunion\\en"));
+        assertNotNull(fileChooserENtest.getInitialDirectory());
+    }
+
+    @Test
+    public void checkInitialDirectoryDK() {
+        fileChooserDKtest.setInitialDirectory(new File("D:\\Applications\\SpaceConqueror\\src\\main\\resources\\org\\snorri1986\\spaceconqueror\\content\\text\\sovietunion\\dk"));
+        assertNotNull(fileChooserDKtest.getInitialDirectory());
+    }
+}
