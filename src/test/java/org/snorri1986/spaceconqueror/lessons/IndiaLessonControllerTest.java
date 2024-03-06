f6f07559bfa3f3b3d508ba0435947884ff8853b3
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-03-06 21:16:37 +0100

    #50 unit tests for Satellites - India

--- src/test/java/org/snorri1986/spaceconqueror/lessons/IndiaLessonControllerTest.java
+++ src/test/java/org/snorri1986/spaceconqueror/lessons/IndiaLessonControllerTest.java
@@ -1,5 +1,6 @@
 package org.snorri1986.spaceconqueror.lessons;
 
+import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
@@ -47,5 +48,29 @@ public class IndiaLessonControllerTest extends ApplicationTest {
     public void checkInitialDirectoryDK() {
         fileChooserDKtest.setInitialDirectory(new File("D:\\Applications\\SpaceConqueror\\src\\main\\resources\\org\\snorri1986\\spaceconqueror\\content\\text\\india\\dk"));
         assertNotNull(fileChooserDKtest.getInitialDirectory());
+    }
+    @Test
+    public void testIndiaSatellitesPhotoOne() {
+        String indiaSatellitesPhotoOnePath = "D:\\Applications\\SpaceConqueror\\src\\main\\resources\\org\\snorri1986\\spaceconqueror\\content\\photos\\india\\satellites\\1.jpg";
+        File photoIndiaSatellitesOneFile = new File(indiaSatellitesPhotoOnePath);
+        Image photoIndiaSatellitesOneImage = new Image(photoIndiaSatellitesOneFile.toURI().toString());
+        assertNotNull(photoIndiaSatellitesOneFile);
+        assertNotNull(photoIndiaSatellitesOneImage);
+    }
+    @Test
+    public void testIndiaSatellitesPhotoTwo() {
+        String indiaSatellitesPhotoTwoPath = "D:\\Applications\\SpaceConqueror\\src\\main\\resources\\org\\snorri1986\\spaceconqueror\\content\\photos\\india\\satellites\\2.jpg";
+        File photoIndiaSatellitesTwoFile = new File(indiaSatellitesPhotoTwoPath);
+        Image photoIndiaSatellitesTwoImage = new Image(photoIndiaSatellitesTwoFile.toURI().toString());
+        assertNotNull(photoIndiaSatellitesTwoFile);
+        assertNotNull(photoIndiaSatellitesTwoImage);
+    }
+    @Test
+    public void testIndiaSatellitesPhotoThree() {
+        String indiaSatellitesPhotoThreePath = "D:\\Applications\\SpaceConqueror\\src\\main\\resources\\org\\snorri1986\\spaceconqueror\\content\\photos\\india\\satellites\\3.jpg";
+        File photoIndiaSatellitesThreeFile = new File(indiaSatellitesPhotoThreePath);
+        Image photoIndiaSatellitesThreeImage = new Image(photoIndiaSatellitesThreeFile.toURI().toString());
+        assertNotNull(photoIndiaSatellitesThreeFile);
+        assertNotNull(photoIndiaSatellitesThreeImage);
     }
 }
