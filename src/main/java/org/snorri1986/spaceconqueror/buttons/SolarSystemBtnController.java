f1bf15656142cc1df937bc3b2dd8f2dba36a6e63
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-05-13 20:14:05 +0200

    #46 configuration - PVS-Studio

--- src/main/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnController.java
+++ src/main/java/org/snorri1986/spaceconqueror/buttons/SolarSystemBtnController.java
@@ -1,3 +1,5 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 package org.snorri1986.spaceconqueror.buttons;
 
 import javafx.fxml.FXML;
