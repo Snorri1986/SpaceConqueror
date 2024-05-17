45b380586bb9d1b4d70ce1c88e87b5ca0d2c24bc
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-05-17 20:29:24 +0200

    #46 file SovietUnionBtnControllerTest final done  - Checkstyle

--- src/test/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnControllerTest.java
+++ src/test/java/org/snorri1986/spaceconqueror/buttons/SovietUnionBtnControllerTest.java
@@ -1,6 +1,8 @@
-// pvs-studio exclude file
-// This file will be excluded from analysis
 package org.snorri1986.spaceconqueror.buttons;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
 
 import javafx.application.Platform;
 import javafx.scene.Scene;
@@ -12,172 +14,169 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(ApplicationExtension.class)
 public class SovietUnionBtnControllerTest extends ApplicationTest {
 
-    public final AppLauncherController appLauncherController = new AppLauncherController();
-    public final SovietUnionBtnController sovietUnionBtnController = new SovietUnionBtnController();
+  public final AppLauncherController appLauncherController = new AppLauncherController();
+  public final SovietUnionBtnController sovietUnionBtnController = new SovietUnionBtnController();
 
-    @Start
-    public void start(Stage stageOriginal) throws IOException {
-        stageOriginal = appLauncherController.setSovietUnionStage(appLauncherController.setSovietUnionScene());
-        stageOriginal.show();
-    }
+  @Start
+  public void start(Stage stageOriginal) throws IOException {
+    stageOriginal = appLauncherController.setSovietUnionStage(appLauncherController.setSovietUnionScene());
+    stageOriginal.show();
+  }
 
-    @Test
-    void testVostokStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testVostokScene = null;
-                try {
-                    testVostokScene = sovietUnionBtnController.setVostokScene();
-                    Stage testVostokStage = sovietUnionBtnController.setVostokStage(testVostokScene);
-                    assertNotNull(testVostokStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testVostokStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testVostokScene = null;
+        try {
+          testVostokScene = sovietUnionBtnController.setVostokScene();
+          Stage testVostokStage = sovietUnionBtnController.setVostokStage(testVostokScene);
+          assertNotNull(testVostokStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testVoshodStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testVoshodScene = null;
-                try {
-                    testVoshodScene = sovietUnionBtnController.setVoshodScene();
-                    Stage testVoshodStage = sovietUnionBtnController.setVoshodStage(testVoshodScene);
-                    assertNotNull(testVoshodStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testVoshodStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testVoshodScene = null;
+        try {
+          testVoshodScene = sovietUnionBtnController.setVoshodScene();
+          Stage testVoshodStage = sovietUnionBtnController.setVoshodStage(testVoshodScene);
+          assertNotNull(testVoshodStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testSoyuzStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testSoyuzScene = null;
-                try {
-                    testSoyuzScene = sovietUnionBtnController.setSoyuzScene();
-                    Stage testSoyuzStage = sovietUnionBtnController.setSoyuzStage(testSoyuzScene);
-                    assertNotNull(testSoyuzStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testSoyuzStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testSoyuzScene = null;
+        try {
+          testSoyuzScene = sovietUnionBtnController.setSoyuzScene();
+          Stage testSoyuzStage = sovietUnionBtnController.setSoyuzStage(testSoyuzScene);
+          assertNotNull(testSoyuzStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testMirStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testMirScene = null;
-                try {
-                    testMirScene = sovietUnionBtnController.setMirScene();
-                    Stage testMirStage = sovietUnionBtnController.setMirStage(testMirScene);
-                    assertNotNull(testMirStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testMirStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testMirScene = null;
+        try {
+          testMirScene = sovietUnionBtnController.setMirScene();
+          Stage testMirStage = sovietUnionBtnController.setMirStage(testMirScene);
+          assertNotNull(testMirStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testBuranStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testBuranScene = null;
-                try {
-                    testBuranScene = sovietUnionBtnController.setBuranScene();
-                    Stage testBuranStage = sovietUnionBtnController.setBuranStage(testBuranScene);
-                    assertNotNull(testBuranStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testBuranStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testBuranScene = null;
+        try {
+          testBuranScene = sovietUnionBtnController.setBuranScene();
+          Stage testBuranStage = sovietUnionBtnController.setBuranStage(testBuranScene);
+          assertNotNull(testBuranStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testLunaStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testLunaScene = null;
-                try {
-                    testLunaScene = sovietUnionBtnController.setLunaScene();
-                    Stage testLunaStage = sovietUnionBtnController.setLunaStage(testLunaScene);
-                    assertNotNull(testLunaStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testLunaStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testLunaScene = null;
+        try {
+          testLunaScene = sovietUnionBtnController.setLunaScene();
+          Stage testLunaStage = sovietUnionBtnController.setLunaStage(testLunaScene);
+          assertNotNull(testLunaStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testVeneraStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testVeneraScene = null;
-                try {
-                    testVeneraScene = sovietUnionBtnController.setVeneraScene();
-                    Stage testVeneraStage = sovietUnionBtnController.setVeneraStage(testVeneraScene);
-                    assertNotNull(testVeneraStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testVeneraStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testVeneraScene = null;
+        try {
+          testVeneraScene = sovietUnionBtnController.setVeneraScene();
+          Stage testVeneraStage = sovietUnionBtnController.setVeneraStage(testVeneraScene);
+          assertNotNull(testVeneraStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testVegaStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testVegaScene = null;
-                try {
-                    testVegaScene = sovietUnionBtnController.setVegaScene();
-                    Stage testVegaStage = sovietUnionBtnController.setVegaStage(testVegaScene);
-                    assertNotNull(testVegaStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testVegaStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testVegaScene = null;
+        try {
+          testVegaScene = sovietUnionBtnController.setVegaScene();
+          Stage testVegaStage = sovietUnionBtnController.setVegaStage(testVegaScene);
+          assertNotNull(testVegaStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 
-    @Test
-    void testMarsStage() throws IOException {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Scene testMarsScene = null;
-                try {
-                    testMarsScene = sovietUnionBtnController.setMarsScene();
-                    Stage testMarsStage = sovietUnionBtnController.setMarsStage(testMarsScene);
-                    assertNotNull(testMarsStage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
+  @Test
+  void testMarsStage() throws IOException {
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        Scene testMarsScene = null;
+        try {
+          testMarsScene = sovietUnionBtnController.setMarsScene();
+          Stage testMarsStage = sovietUnionBtnController.setMarsStage(testMarsScene);
+          assertNotNull(testMarsStage);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
+  }
 }
